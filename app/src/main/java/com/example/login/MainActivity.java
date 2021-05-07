bc7edccfbd31a69f565622173fd3b60f30d61f30
jnuwxc <1165124074@qq.com>
2021-05-07 17:50:16 +0800

    修改MainAcitivity部分逻辑

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -24,13 +24,19 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-//        SharedPreferences loginSP = getSharedPreferences("login", Context.MODE_PRIVATE);
-//        boolean isLogin = loginSP.getBoolean("isLogin", false);
-//        if(isLogin){
-//            Toast.makeText(this, "user info: " + loginSP.getString("userName", "error"), Toast.LENGTH_SHORT).show();
-//        }else {
-//            Intent intent = new Intent(this, LoginActivity.class);
-//            startActivity(intent);
-//        }
+        SharedPreferences loginSP = getSharedPreferences("login", Context.MODE_PRIVATE);
+        boolean isLogin = loginSP.getBoolean("isLogin", false);
+        if(isLogin){
+            Toast.makeText(this, "欢迎用户: " + loginSP.getString("account", "error"), Toast.LENGTH_SHORT).show();
+            SharedPreferences userInfoSp = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+            if("error".equals(userInfoSp.getString("account", "error"))){
+                // 向服务器发送请求，获取用户信息，并存入到userInfo中
+            }else {
+                // 从sp中获取相应信息并设置到界面上
+            }
+        }else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
